c61a6a0d473210019b9bc2678af306d7631ae3b0
Wilson Macuacua <wmacuacua@nef.co.mz>
2024-01-11 16:02:18 +0200

    bulding Game

--- game.py
+++ game.py
@@ -6,3 +6,19 @@ def limpa_tela():
         _= system('cls')
     else:
         _= system('clear')
+
+
+
+ #Funcao 
+def game():
+
+    limpa_tela()
+    limpa_tela()
+    print("\nBem-Vindo ao jogo da forca!")
+    print("Advinhe a palavra abaixo:\n")
+
+    # Lista de palavras para o jogo
+    palavras = ['banana', 'abacate', 'uva', 'morango', 'laranja']
+
+    palavra = random.choice(palavras)
+            
